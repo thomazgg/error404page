5c9e18ce99b2dade0fd82fdd890195500189240f
Thomaz <79727201+thomazgg@users.noreply.github.com>
2021-08-21 00:12:11 -0300

    Add files via upload

---
+++ error404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Oops... Parece que você se perdeu!</title>
+
+        <link rel="stylesheet" href="style.css">
+    </style>
+
+</head>
+    <body class="bg-purple">
+        
+        <div class="stars">
+            <div class="central-body">   
+
+                <img class="image-404" src="img/404.png" width="500px">
+                
+                    <h1>Parece que você se perdeu no espaço</h1>
+                    <h3>A página que você está tentando acessar não existe ou foi movida. <br> Tente voltar à nossa página inicial.</h3>
+                    <a href="http://localhost/FIB2021/Websites/Discord" class="btn-go-home">VOLTAR PRA CASA</a>
+                </div>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="img/rocket.svg" width="40px">
+                <div class="earth-moon">
+                    <img class="object_earth" src="img/earth.svg" width="100px">
+                    <img class="object_moon" src="img/moon.svg" width="80px">
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src="img/astronaut.svg" width="140px">
+                </div>
+                
+                <div class="error404">
+                    <img class="alien" src="img/alien.png"></img>
+                    <img class="ufo" src="img/ufo.png"></img>
+                    <!-- 
+                    <img class="estela" src="img/estela.png"></img>
+                    -->
+                    <img class="satelite" src="img/satelite.png"></div>
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+
+            </div>
+        </div>
+
+    </body>
+</html>
